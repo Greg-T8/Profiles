637190d02dc5b0a748ab57a6a57b52dc58e443de
Greg Tate <gregt8@outlook.com>
2025-06-12 05:15:28 -0500

    fix: add missing newline for improved readability in profile.ps1

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -29,6 +29,7 @@ if (Test-Path -Path ./functions.ps1) {
 
 if (Test-Path -Path $env:OneDriveCommercial/Code/WorkConfig.psd1) {
     $Work = Import-PowerShellDataFile -Path $env:OneDriveCommercial/Code/WorkConfig.psd1
+
     $orderedAccounts = [ordered]@{}
     foreach ($key in $Work.Accounts.Keys | Sort-Object) {
         $orderedAccounts[$key] = $Work.Accounts[$key]
