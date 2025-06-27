682fb95d2add52c86730c549b14abb0aa444cb8e
Greg Tate <gregt8@outlook.com>
2025-06-27 05:19:53 -0500

    add: integrate mini.surround plugin with key mappings for surrounding management

--- nvim/lua/_common/plugins/plugins.lua
+++ nvim/lua/_common/plugins/plugins.lua
@@ -10,4 +10,24 @@ return {
             vim.keymap.set('o', 'S', '<Plug>(leap-backward)')
         end,
     },
+    {
+        'echasnovski/mini.surround',
+        config = function()
+            require('mini.surround').setup({
+                mappings = {
+                    add = "<leader>sa", -- Add surrounding in Normal and Visual modes
+                    delete = "<leader>sd", -- Delete surrounding
+                    find = "<leaderf>sf", -- Find surrounding (to the right)
+                    find_left = "<leader>sF", -- Find surrounding (to the left)
+                    highlight = "<leader>sh", -- Highlight surrounding
+                    replace = "<leader>sr", -- Replace surrounding
+                    update_n_lines = "<leader>sn", -- Update `n_lines`
+                },
+                -- Whether to respect selection type:
+                -- - Place surroundings on separate lines in linewise mode.
+                -- - Place surroundings on each line in blockwise mode.
+                respect_selection_type = false,
+            })
+        end,
+    },
 }
