c4849df6374d817d9587ebd09f28ed6b0ab8b4f7
Greg Tate <gregt8@outlook.com>
2025-07-11 04:32:54 -0500

    update: update leap.nvim commit and refine key mappings in keymaps.lua

--- nvim/lua/_common/keymaps.lua
+++ nvim/lua/_common/keymaps.lua
@@ -20,16 +20,20 @@ vim.keymap.set("n", "<C-q>", "<C-v>", { desc = "Enter visual block mode", norema
 vim.keymap.set('n', 'G', 'G$', { desc = 'Go to end of line and center' })
 
 -- Add keymapping for centering cursor
--- vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center cursor on screen' })
--- vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page down and center' })
--- vim.keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'Page down and center' })
--- vim.keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'Page up and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Center cursor on screen' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Half page down and center' })
+vim.keymap.set('n', '<C-f>', '<C-f>zz', { desc = 'Page down and center' })
+vim.keymap.set('n', '<C-b>', '<C-b>zz', { desc = 'Page up and center' })
 
 -- Insert mode --
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Quick exit from insert mode", noremap = true, silent = true })
 
-
 -- Visual mode --
+
+-- Simulate page-size scrolling (haven't gotten this to work yet)
+-- vim.keymap.set('v', '<C-e>', '40<C-e>', { desc = 'Scroll down in visual mode', noremap = true, silent = true })
+-- vim.keymap.set('v', '<C-y>', '40<C-y>', { desc = 'Scroll up in visual mode', noremap = true, silent = true })
+
 -- Stay in indent mode
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and keep selection", noremap = true, silent = true })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and keep selection", noremap = true, silent = true })
