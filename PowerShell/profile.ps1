ae50dbd476e625fcc0667763984942c7804d4fdd
Greg Tate <gregt8@outlook.com>
2025-08-03 06:03:20 -0500

    update: enhance PSReadLine options for improved prediction and view style

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -60,7 +60,7 @@ Remove-Item Alias:dir
 # Enable keyboard shortcuts
 if (-not (Get-Module PSReadline)) { Import-Module PSReadLine }
 Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin -PredictionViewStyle ListView
 Set-PSReadLineKeyHandler -Chord Tab -Function TabCompleteNext
 Set-PSReadLineKeyHandler -Chord Shift+Tab -Function TabCompletePrevious
 Set-PSReadLineKeyHandler -Chord Ctrl+V -Function Paste
