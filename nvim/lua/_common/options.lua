a11b1fc4fb5a593ad85ca06976ccdbac1a1fff32
Greg Tate <gregt8@outlook.com>
2025-10-16 07:44:52 -0500

    Notes: fix formatting in options.lua for consistency and clarity

--- nvim/lua/_common/options.lua
+++ nvim/lua/_common/options.lua
@@ -1,4 +1,5 @@
 -- Use leader key to enter visual block mode
-vim.api.nvim_set_keymap('n', '<leader>vb',
-"<C-v>",
-{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>vb', "<C-v>", { noremap = true, silent = true })
+
+-- Clear search highlighting
+vim.api.nvim_set_keymap('n', '<leader>n', ':nohlsearch<CR>', { noremap = true, silent = true })
